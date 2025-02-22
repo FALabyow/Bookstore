f8f638ffc9d6859d9b51f955f62c4ced070eff84
FALabyow <ruhaynaadje08@gmail.com>
2025-02-22 16:38:46 +0800

    Added the API

---
+++ Bookstore.API/Controllers/BooksController.cs
@@ -0,0 +1,22 @@
+﻿using Bookstore.Application.Interfaces;
+using Bookstore.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookstore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookRepository _repository;
+
+        public BooksController(IBookRepository repository) => _repository = repository;
+
+        [HttpGet] public async Task<IActionResult> GetAll() => Ok(await _repository.GetAllAsync());
+        [HttpGet("{id}")] public async Task<IActionResult> Get(int id) => Ok(await _repository.GetByIdAsync(id));
+        [HttpPost] public async Task<IActionResult> Add([FromBody] Book book) { await _repository.AddAsync(book); return Ok(); }
+        [HttpPut] public async Task<IActionResult> Update([FromBody] Book book) { await _repository.UpdateAsync(book); return Ok(); }
+        [HttpDelete("{id}")] public async Task<IActionResult> Delete(int id) { await _repository.DeleteAsync(id); return Ok(); }
+    }
+}
+
---
+++ Bookstore.API/Program.cs
@@ -0,0 +1,44 @@
+
+using Bookstore.Application.Interfaces;
+using Bookstore.Infrastructure.Persistence;
+using Bookstore.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookstore.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AppDbContextConnection")));
+            builder.Services.AddScoped<IBookRepository, BookRepository>();
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
