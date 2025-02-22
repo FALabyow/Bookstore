9bd1796580b356f37e7e454ae8f55699fd703971
FALabyow <ruhaynaadje08@gmail.com>
2025-02-22 16:18:23 +0800

    Added the application layer

---
+++ Bookstore.Application/Interfaces/IBookRepository.cs
@@ -0,0 +1,18 @@
+﻿using Bookstore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookstore.Application.Interfaces
+{
+    public interface IBookRepository
+    {
+        Task<IEnumerable<Book>> GetAllAsync();
+        Task<Book> GetByIdAsync(int id);
+        Task AddAsync(Book book);
+        Task UpdateAsync(Book book);
+        Task DeleteAsync(int id);
+    }
+}
