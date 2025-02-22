f405f0996a64392ea094186b489c96a4efc05f59
FALabyow <ruhaynaadje08@gmail.com>
2025-02-22 16:24:12 +0800

    Added the infrastructure layer

---
+++ Bookstore.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Bookstore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookstore.Infrastructure.Persistence
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ Bookstore.Infrastructure/Repositories/BookRepository.cs
@@ -0,0 +1,24 @@
+﻿using Bookstore.Application.Interfaces;
+using Bookstore.Domain.Entities;
+using Bookstore.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookstore.Infrastructure.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly AppDbContext _context;
+        public BookRepository(AppDbContext context) => _context = context;
+
+        public async Task<IEnumerable<Book>> GetAllAsync() => await _context.Books.ToListAsync();
+        public async Task<Book> GetByIdAsync(int id) => await _context.Books.FindAsync(id);
+        public async Task AddAsync(Book book) { _context.Books.Add(book); await _context.SaveChangesAsync(); }
+        public async Task UpdateAsync(Book book) { _context.Books.Update(book); await _context.SaveChangesAsync(); }
+        public async Task DeleteAsync(int id) { var book = await _context.Books.FindAsync(id); if (book != null) { _context.Books.Remove(book); await _context.SaveChangesAsync(); } }
+    }
+}
